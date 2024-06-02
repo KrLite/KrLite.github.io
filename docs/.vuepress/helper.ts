c8df3f5d6975839831638a61b9dd822d53f8c86c
KrLite <68179735+KrLite@users.noreply.github.com>
2024-06-02 18:03:15 +0800

    Update helper.ts

--- docs/.vuepress/helper.ts
+++ docs/.vuepress/helper.ts
@@ -1,5 +1,5 @@
 export const isDev = process.env.NODE_ENV === "development";
-export const baseUrl = isDev ? "/" : "/Press/Press/";
+export const baseUrl = isDev ? "/" : "/Press/";
 
 export function withBase(path: string): string {
   return `${baseUrl}${path}`.replace("//", "/");
