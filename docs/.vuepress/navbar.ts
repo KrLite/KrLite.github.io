8074c141b78fed33181783ee635df1b53d418b37
KrLite <68179735+KrLite@users.noreply.github.com>
2024-06-02 16:53:39 +0800

    ?

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,5 +1,5 @@
 import { defineNavbarConfig } from "vuepress-theme-plume";
 
 export default defineNavbarConfig([
-  { text: "友链", link: "//friends/", icon: "solar:compass-bold" },
+  { text: "友链", link: " /friends/", icon: "solar:compass-bold" },
 ]);
