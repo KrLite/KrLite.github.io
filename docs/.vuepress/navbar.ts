06019dd0db0cc1751a14ab41c29dbb01b300214e
KrLite <68179735+KrLite@users.noreply.github.com>
2024-06-02 18:20:23 +0800

    E

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,5 +1,6 @@
 import { defineNavbarConfig } from "vuepress-theme-plume";
 
 export default defineNavbarConfig([
+  { text: "所有文章", link: "/blog/", icon: "solar:document-text-bold" },
   { text: "友链", link: "/friends/", icon: "solar:compass-bold" },
 ]);
---
+++ docs/articles/2024/6/hello_world.md
@@ -0,0 +1,10 @@
+---
+title: Hello World!
+author: KrLite
+createTime: 2024/06/02 18:15:02
+permalink: /article/ejmlfxil/
+---
+
+```typescript
+console.log("Hello World!");
+```
