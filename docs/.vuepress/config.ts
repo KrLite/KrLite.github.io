b5758aed77977b6e05a68e87efe4cc48e1bb79b0
KrLite <68179735+KrLite@users.noreply.github.com>
2024-06-02 13:15:39 +0800

    Workflows

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,32 @@
-import { defineUserConfig } from 'vuepress'
-import { viteBundler } from '@vuepress/bundler-vite'
-import { plumeTheme } from 'vuepress-theme-plume'
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
+import { plumeTheme } from "vuepress-theme-plume";
 
 export default defineUserConfig({
-  lang: 'zh-CN',
-  theme: plumeTheme({
+  base: "/Press/",
 
+  lang: "zh-CN",
+  title: "Press",
+  description: "Once a fantacy unconjectured",
+
+  theme: plumeTheme({
+    avatar: {
+      url: "https://github.com/KrLite.png",
+      name: "KrLite",
+      description: "坚守此岸。",
+      circle: true,
+    },
+    plugins: {
+      comment: {
+        provider: "Giscus",
+        comment: true,
+        repo: "KrLite/Press",
+        repoId: "R_kgDOMDumMA",
+        category: "Announcements",
+        categoryId: "DIC_kwDOMDumMM4Cfys_",
+      },
+    },
   }),
+
   bundler: viteBundler(),
-})
+});
