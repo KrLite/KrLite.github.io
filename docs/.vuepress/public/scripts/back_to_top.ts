76a3c056f5c3c3ee4a7657128d3e4020240d3ea2
KrLite <68179735+KrLite@users.noreply.github.com>
2024-06-08 23:21:44 +0800

    Back to top button restructured

--- docs/.vuepress/public/scripts/back_to_top.ts
+++ docs/.vuepress/public/scripts/back_to_top.ts
@@ -7,19 +7,19 @@ export function overrideBackToTopButton() {
       button.data("initialized", true);
       console.log("Initializing back to top button");
 
-      //button.children().remove();
+      button.children().remove("svg");
       const svg = button
-        .append("<svg aria-hidden='true'></svg>")
+        .append("<svg aria-hidden='true'><circle cx='50%' cy='50%'></circle></svg>")
         .children("svg");
-      const circle = svg
-        .append("<circle></circle>")
-        .children("circle");
+      const circle = svg.children("circle");
 
-      $(document.body).scroll(function () {
-        const scrollTop = document.body.scrollTop;
+      $(window).scroll(function () {
+        const scrollTop = window.scrollY;
         const scrollableHeight =
           document.body.scrollHeight - window.innerHeight;
         const percent = Math.min(1, Math.max(0, scrollTop / scrollableHeight));
+
+        button.css("--percent", percent);
       });
     }
   });
