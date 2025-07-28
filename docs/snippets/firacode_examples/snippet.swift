ab2f728f9b931d12f229cdc7f03d603c06984656
KrLite <wang070621@foxmail.com>
2025-07-28 11:04:06 +0800

    Update snippet.swift

--- docs/snippets/firacode_examples/snippet.swift
+++ docs/snippets/firacode_examples/snippet.swift
@@ -1,13 +1,13 @@
 let closure: (Int?) -> String = {
-	return "Number: \($0 ?? 0)"
+    "Number: \($0 ?? 0)"
 }
 
-for i in 1..<100 {
-	guard i != 2 else { continue }
+for i in 1 ..< 100 {
+    guard i != 2 else { continue }
 
-	if i == 42 {
-		print(closure(i * 10))
-	} else if i >= 50 || i <= 75 {
-		print(closure(i))
-	}
+    if i == 42 {
+        print(closure(i * 10))
+    } else if i >= 50 || i <= 75 {
+        print(closure(i))
+    }
 }
