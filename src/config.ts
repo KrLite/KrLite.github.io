9180cfa225001e3549442ebc9dfd469272574279
KrLite <wang070621@foxmail.com>
2025-07-28 14:10:26 +0800

    ♻️ Update website link

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
+    website: 'https://krlite.netlify.app/', // Site domain
     title: 'Facts About KrLite', // Site title
     author: 'KrLite', // Author name
     source: 'https://github.com/KrLite',
